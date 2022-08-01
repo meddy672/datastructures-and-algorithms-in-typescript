59ff35b980ed5d6ffd9905275ba1f7d8edfc9438
Matthew Eddy <meddy672@gmail.com>
2022-08-01 08:20:11 -0400

    updated fizzbuzz

--- challenges/fizzbuzz.ts
+++ challenges/fizzbuzz.ts
@@ -4,41 +4,63 @@
  * - If the number is dvisible by three and 5 log out fizzbuzz instead of that number
  * - If the number is not divisble by 3 o 5 just log out the number
  */
-
-function fizzBuzz({ num, divisor1, divisor2 }: IFizzBuzz): void {
-     if (isNotZero(divisor1, divisor2)) {
-
-          enum Remainder { ZERO = 0 };
-
-          if (num === 0) {
-               return;
-          } else {
-               if ((num % divisor1) === Remainder.ZERO && (num % divisor2) === Remainder.ZERO) console.log('fizzbuzz');
-               else if ((num % divisor1) === Remainder.ZERO) console.log('fizz');
-               else if ((num % divisor2) === Remainder.ZERO) console.log('buzz');
-               else console.log(num);
-               num--;
-               fizzBuzz({
-                    num,
-                    divisor1,
-                    divisor2
-               });
-          }
-     } else console.log(`Divisors:(${divisor1}, ${divisor2}) cannot have a zero value.`)
-}
-
-function isNotZero(divisor1: number, divisor2: number): Boolean {
-     return divisor1 !== 0 && divisor2 !== 0;
-}
+enum Remainder { ZERO = 0 };
 
 interface IFizzBuzz {
      num: number;
      divisor1: number;
-     divisor2: number
+     divisor2: number;
+     context: {
+          fizz: string;
+          buzz: string;
+          fizzbuzz: string;
+     }
 }
 
 fizzBuzz({
      num: 20,
      divisor1: 3,
-     divisor2: 6
+     divisor2: 6,
+     context: {
+          fizz: 'fizz',
+          buzz: 'buzz',
+          fizzbuzz: 'fizzbuzz'
+     }
 });
+
+
+/**
+ * 
+ * @param {IFizzBuzz} -  Object for configuring fizzBuzz function
+ * @returns 
+ */
+function fizzBuzz({ num, divisor1, divisor2, context }: IFizzBuzz): void {
+     if (isNotZero(divisor1, divisor2)) {
+
+          if (num === 0) {
+               return;
+          } else {
+               if ((num % divisor1) === Remainder.ZERO 
+                    && (num % divisor2) === Remainder.ZERO) console.log(context.fizzbuzz);
+               else if ((num % divisor1) === Remainder.ZERO) console.log(context.fizz);
+               else if ((num % divisor2) === Remainder.ZERO) console.log(context.buzz);
+               else console.log(num);
+               num--;
+               fizzBuzz({
+                    num,
+                    divisor1,
+                    divisor2,
+                    context
+               });
+          }
+     } else console.log(`Divisors:(${divisor1}, ${divisor2}) cannot have a zero value.`)
+}
+
+/**
+ * @param {number} divisor1 - divisior to determine fizz
+ * @param {number} divisor2 - divisor to determine buzz
+ * @returns {Boolean}
+ */
+function isNotZero(divisor1: number, divisor2: number): Boolean {
+     return divisor1 !== 0 && divisor2 !== 0;
+}
