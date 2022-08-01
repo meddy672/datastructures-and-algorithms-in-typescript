788a34af6749105e9f5d86cff07d37a0698f3b46
Matthew Eddy <meddy672@gmail.com>
2022-08-01 08:04:47 -0400

    updated fizzbuzz

--- challenges/fizzbuzz.ts
+++ challenges/fizzbuzz.ts
@@ -5,33 +5,40 @@
  * - If the number is not divisble by 3 o 5 just log out the number
  */
 
-function fizzBuzz({num, divisor1, divisor2}: IFizzBuzz ): void {
-   enum Remainder { ZERO = 0 };
+function fizzBuzz({ num, divisor1, divisor2 }: IFizzBuzz): void {
+     if (isNotZero(divisor1, divisor2)) {
 
-   if (num === 0) {
-        return;
-   } else {
-        if ((num % divisor1) === 0 && (num % divisor2) === 0) console.log('fizzbuzz');
-        else if ((num % divisor1) === 0) console.log('fizz');
-        else if ((num % divisor2) === 0) console.log('buzz');
-        else console.log(num);
-        num--;
-        fizzBuzz({
-          num,
-          divisor1,
-          divisor2
-        });
-   }
+          enum Remainder { ZERO = 0 };
+
+          if (num === 0) {
+               return;
+          } else {
+               if ((num % divisor1) === Remainder.ZERO && (num % divisor2) === Remainder.ZERO) console.log('fizzbuzz');
+               else if ((num % divisor1) === Remainder.ZERO) console.log('fizz');
+               else if ((num % divisor2) === Remainder.ZERO) console.log('buzz');
+               else console.log(num);
+               num--;
+               fizzBuzz({
+                    num,
+                    divisor1,
+                    divisor2
+               });
+          }
+     } else console.log(`Divisors:(${divisor1}, ${divisor2}) cannot have a zero value.`)
+}
+
+function isNotZero(divisor1: number, divisor2: number): Boolean {
+     return divisor1 !== 0 && divisor2 !== 0;
 }
 
 interface IFizzBuzz {
      num: number;
      divisor1: number;
      divisor2: number
-} 
+}
 
 fizzBuzz({
-    num: 20,
-    divisor1: 3,
-    divisor2: 5
+     num: 20,
+     divisor1: 3,
+     divisor2: 6
 });
